560bd5013084eb4c86230cc62c4d8d5c3c128a23
Todor Evtimov <todor.ev@gmail.com>
2023-11-17 10:41:17 +0200

    Github task: Expose compare and Open PR buttons

--- src/pages/Content/index.tsx
+++ src/pages/Content/index.tsx
@@ -53,3 +53,21 @@ onLoad()
 
 // TODO: find a better way to recheck for page changes
 document.addEventListener('scroll', onLoad)
+
+function exposeCompareAndOpenPullRequestButtons() {
+  if (!location.href.match(/https:\/\/github.com/)) return
+  // Expose Compare and Open pull request buttons in branch summary
+  const contributeBlock = findEl(`//div[details/summary[text()[contains(.,"Contribute")]]]`) as HTMLDivElement | null
+
+  const contributeButtonsBlock = findEl(`//div/div/ul/li/div[a[text()[contains(.,"Open pull request")]]]`)
+
+  contributeButtonsBlock && contributeBlock?.appendChild(contributeButtonsBlock)
+
+  if (contributeBlock) {
+    const contributeDetailsBlock = findEl(`details`, contributeBlock) as HTMLDivElement
+
+    contributeDetailsBlock && (contributeDetailsBlock.style.display = 'none')
+  }
+}
+
+exposeCompareAndOpenPullRequestButtons()
