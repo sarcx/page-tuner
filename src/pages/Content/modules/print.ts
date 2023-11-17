9cd8f1c796291b749aff41487e91dd1a5c07054e
Todor Evtimov <todor.ev@gmail.com>
2023-11-17 09:41:20 +0200

    Github Task: Minimize/review autogenerated files
    There are a bunch of autogenerated files that in most of the cases are
    just skipped.

---
+++ src/pages/Content/modules/print.ts
@@ -0,0 +1,30 @@
+export const printLine = (line: any) => {
+  console.log('===>', line)
+}
+
+export function findEl(pathToNode: string, within?: Node | null) {
+  // TODO add some loging when within is empty
+  return document.evaluate(pathToNode, within ? within : document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+    ?.singleNodeValue
+}
+
+export function clickOn(el: HTMLElement | null) {
+  if (!el) return
+
+  if (!el.classList.contains('pt-clicked')) {
+    console.log('PT-Action: click')
+    el.click()
+    el.classList.add('pt-clicked')
+  }
+}
+
+export function markAsClicked(el: HTMLElement) {
+  if (!el.classList.contains('pt-clicked')) {
+    console.log('PT-Action: mark as clicked')
+    el.classList.add('pt-clicked')
+  }
+}
+
+export function isClicked(node: HTMLElement) {
+  return node.classList.contains('pt-clicked')
+}
